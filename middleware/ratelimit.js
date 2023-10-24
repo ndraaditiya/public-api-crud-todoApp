2a2acb8b014961ad997cee143aacbc5d5154c230
Andra <andraaries1998@gmail.com>
2023-10-24 12:40:04 +0700

    update readme

--- middleware/ratelimit.js
+++ middleware/ratelimit.js
@@ -2,15 +2,15 @@ import { rateLimit } from 'express-rate-limit'
 
 export const rateLimitChange = rateLimit({
   windowMs: 60 * 1000,
-  max: 3,
+  max: 5,
   message:
-    'You have exceeded your 3 requests per minute limit for change data.',
+    'You have exceeded your 5 requests per minute limit for change data.',
   headers: true,
 })
 
 export const rateLimitGet = rateLimit({
   windowMs: 1000,
-  max: 1,
-  message: 'You have exceeded your 1 requests per second limit for get data.',
+  max: 2,
+  message: 'You have exceeded your 2 requests per second limit for get data.',
   headers: true,
 })
